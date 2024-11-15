00299a293fded1e14f8aa0add05dacad7ef46b30
Uwakmfon1 <udohuwakmfon2@gmail.com>
2024-11-15 17:47:50 +0100

    modified-user-create-method

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Cassandra\Time;
 use Illuminate\Http\Request;
 //use \Auth
 
@@ -17,24 +18,22 @@ class AuthController extends Controller
 
     public function create(Request $request)
     {
-        $request->validate([
+       $result = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|string|min:8|confirmed'
         ]);
-
-        $user = new User();
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = bcrypt($request->password);
-//        $user->verified_at =
-        $user->save();
-//        try {
-//            User::create($result);
-//            return response()->json(['message'=>'successfully saved to database'],201);
-//        } catch (\Exception $e) {
-//            return response()->json(['message'=>'could not save to database']);
-//        }
+        $result['email_verified_at'] = now();
+        $result['created_at'] = now();
+        $result['updated_at'] = now();
+        $result['password'] = bcrypt($result['password']);
+        
+        try {
+            User::create($result);
+            return response()->json(['message'=>'successfully saved to database'],201);
+        } catch (\Exception $e) {
+            return response()->json(['message'=>'could not save to database']);
+        }
     }
 
 
